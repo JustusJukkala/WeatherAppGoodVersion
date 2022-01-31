4dd31aa4d7da4b9f78b2884d1ee38da43bb5dca2
JustusJukkala <justus.jukkala@student.lab.fi>
2022-01-31 13:13:33 +0200

    Initial Commit

---
+++ WeatherAppGoodVersion/WeatherAppGoodVersionApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppGoodVersionApp.swift
+//  WeatherAppGoodVersion
+//
+//  Created by Justus Jukkala on 31/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppGoodVersionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
